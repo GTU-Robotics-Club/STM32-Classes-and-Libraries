79085c944513ec62037ab5e1b6fd7fccb4655a2b
sarvesh prajapati <sarvesh101p@gmail.com>
2021-04-02 20:26:16 +0530

    updated-mpu-library

--- MPU-Library/MPU6050.cpp
+++ MPU-Library/MPU6050.cpp
@@ -29,67 +29,122 @@ MPU6050::MPU6050(I2C_HandleTypeDef *hi2c) {
 		data = 0x07;
 		HAL_I2C_Mem_Write(this->hi2c, MPU6050_ADDR, SMPLRT_DIV_REG, 1, &data, 1, 1000);
 		/** Set accelerometer configuration */
-		data = 0x02;
+		data = 0x00;
 		HAL_I2C_Mem_Write(this->hi2c, MPU6050_ADDR, ACCEL_CONFIG_REG, 1, &data, 1, 1000);
 		/** Set Gyroscopic configuration */
-		data = 0x01;
+		data = 0x00;
 		HAL_I2C_Mem_Write(this->hi2c, MPU6050_ADDR, GYRO_CONFIG_REG, 1, &data, 1, 1000);
 		this->is_init = true;
+		this->init_time = HAL_GetTick();
+		HAL_I2C_Mem_Read (this->hi2c, MPU6050_ADDR, GYRO_XOUT_H_REG, 1, this->Rec_Data, 6, 1000);
+		this->Gyro_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data [1]);
+		this->Gyro_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data [3]);
+		this->Gyro_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data [5]);
+		error_Gz_min = Gyro_Z_RAW / 131.0;
+		error_Gz_max = Gyro_Z_RAW / 131.0;
+		for(uint16_t i = 0; i < 3000; i++) {
+			HAL_I2C_Mem_Read (this->hi2c, MPU6050_ADDR, GYRO_XOUT_H_REG, 1, this->Rec_Data, 6, 1000);
+			this->Gyro_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data [1]);
+			this->Gyro_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data [3]);
+			this->Gyro_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data [5]);
+			this->error_Gx += Gyro_X_RAW/131.0;
+			this->error_Gy += Gyro_Y_RAW/131.0;
+			this->error_Gz += Gyro_Z_RAW/131.0;
+			// Modification
+			if(error_Gz_max < (Gyro_Z_RAW / 131.0)) {
+				error_Gz_max = Gyro_Z_RAW / 131.0;
+			}
+			if(error_Gz_min > (Gyro_Z_RAW / 131.0)) {
+				error_Gz_min = Gyro_Z_RAW / 131.0;
+			}
+		}
+		error_Gx /= 3000;
+		error_Gy /= 3000;
+		error_Gz /= 3000;
+		this->previous_interval = HAL_GetTick();
 	} else {
 		this->is_init = false;
 	}
 }
 
-void MPU6050::MPU6050_Read_Accel() {
+void MPU6050::MPU6050_update() {
 	HAL_I2C_Mem_Read (hi2c, MPU6050_ADDR, ACCEL_XOUT_H_REG, 1, this->Rec_Data, 6, 1000);
-
 	this->Accel_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data [1]);
 	this->Accel_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data [3]);
 	this->Accel_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data [5]);
 
-	this->Ax = Accel_X_RAW/4096.0;
-	this->Ay = Accel_Y_RAW/4096.0;
-	this->Az = Accel_Z_RAW/4096.0;
-}
+	this->Ax = Accel_X_RAW/16384.0;
+	this->Ay = Accel_Y_RAW/16384.0;
+	this->Az = Accel_Z_RAW/16384.0;
 
-void MPU6050::MPU6050_Read_Gyro() {
-	HAL_I2C_Mem_Read (this->hi2c, MPU6050_ADDR, GYRO_XOUT_H_REG, 1, this->Rec_Data, 6, 1000);
-
+	HAL_I2C_Mem_Read (this->hi2c, MPU6050_ADDR, GYRO_XOUT_H_REG, 1, this->Rec_Data, 6, 100);
 	this->Gyro_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data [1]);
 	this->Gyro_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data [3]);
 	this->Gyro_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data [5]);
+	this->Gx = Gyro_X_RAW/131.0;
+	this->Gy = Gyro_Y_RAW/131.0;
+	this->Gz = Gyro_Z_RAW/131.0;
+	this->Gx -= error_Gx;
+	this->Gy -= error_Gy;
+	if(Gz > error_Gz_min && Gz < error_Gz_max) {
+		this->Gz = 0;
+	} else {
+		this->Gz -= error_Gz;
+	}
+	interval = (HAL_GetTick() - previous_interval) * 0.001;
+	angle_x = (atan2(-Ay,Az) * 180.0) / M_PI;
+	angle_y = (atan2(Ax, sqrt(Ay * Ay + Az * Az)) * 180.0) / M_PI;
+	angle_z += (Gz) * interval;
 
-	this->Gx = Gyro_X_RAW/65.5;
-	this->Gy = Gyro_Y_RAW/65.5;
-	this->Gz = Gyro_Z_RAW/65.5;
+	previous_interval = HAL_GetTick();
+
 }
 
+
 float MPU6050::get_accel_x() {
-	MPU6050_Read_Accel();
+	MPU6050_update();
 	return this->Ax;
 }
 
 float MPU6050::get_accel_y() {
-	MPU6050_Read_Accel();
+	MPU6050_update();
 	return this->Ay;
 }
 
 float MPU6050::get_accel_z() {
-	MPU6050_Read_Accel();
+	MPU6050_update();
 	return this->Az;
 }
 
-float MPU6050::get_gyro_x_angle() {
-	MPU6050_Read_Gyro();
+float MPU6050::get_gyro_x() {
+	MPU6050_update();
 	return this->Gx;
 }
 
-float MPU6050::get_gyro_y_angle() {
-	MPU6050_Read_Gyro();
+float MPU6050::get_gyro_y() {
+	MPU6050_update();
 	return this->Gy;
 }
 
-float MPU6050::get_gyro_z_angle() {
-	MPU6050_Read_Gyro();
+float MPU6050::get_gyro_z() {
+	MPU6050_update();
 	return this->Gz;
 }
+
+float MPU6050::get_gyro_x_angle() {
+	MPU6050_update();
+	return this->angle_x;
+}
+
+float MPU6050::get_gyro_y_angle() {
+	MPU6050_update();
+	return this->angle_y;
+}
+
+float MPU6050::get_gyro_z_angle() {
+	MPU6050_update();
+	return this->angle_z;
+}
+
+
+
--- MPU-Library/MPU6050.h
+++ MPU-Library/MPU6050.h
@@ -18,10 +18,12 @@
 
 #ifndef MPU6050_H
 #define MPU6050_H
+#define RAD_TO_DEG 57.2957
 
 #include <stdio.h>
-#include "stm32f1xx.h"
-#include "stm32f1xx_hal_i2c.h"
+#include "stm32f4xx.h"
+#include "stm32f4xx_hal_i2c.h"
+#include <math.h>
 /** Note the MPU6050_ADDR is 0xD0 / 0xD1 in 7 bits, if we do right shift, we get the desired address i.e. 0x68 / 0x69. */
 
 #define MPU6050_ADDR 0xD0
@@ -41,19 +43,25 @@ public:
 	float get_accel_x();
 	float get_accel_y();
 	float get_accel_z();
+	float get_gyro_x();
+	float get_gyro_y();
+	float get_gyro_z();
 	float get_gyro_x_angle();
 	float get_gyro_y_angle();
 	float get_gyro_z_angle();
-	void MPU6050_Read_Accel();
-	void MPU6050_Read_Gyro();
+	void MPU6050_update();
 private:
 	I2C_HandleTypeDef* hi2c;
-	float Ax, Ay, Az, Gx, Gy, Gz;
+	float Ax, Ay, Az, Gx, Gy, Gz, angle_x = 0, angle_y = 0, angle_z = 0, interval;
 	int16_t Accel_X_RAW = 0, Accel_Y_RAW = 0, Accel_Z_RAW = 0;
 	int16_t Gyro_X_RAW = 0, Gyro_Y_RAW = 0, Gyro_Z_RAW = 0;
 	uint8_t check, data;
 	uint8_t Rec_Data[6];
+	uint32_t init_time = 0;
+	float error_Gx, error_Gy, error_Gz, error_Gz_min, error_Gz_max;
+	float calculated_z_angle = 0;
 	char buf[32];
+	uint32_t previous_interval;
 };
 
 #endif
